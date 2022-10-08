500c641a7fc82ddbb1470f4283dbc84f2d6abb96
AMITH VISHNU <64186109+amithvishnu@users.noreply.github.com>
2022-10-07 20:31:27 -0400

    Add files via upload

---
+++ get_repository.py
@@ -0,0 +1,49 @@
+"""
+Created by Shashank Ramesh KUmar
+October 07, 2022
+This makes the data requests from GitHub API.
+"""
+
+import requests
+# import pip._vendor.requests as requests
+import json
+
+
+def connect(user):
+    code200 = True
+    repo_url = "https://api.github.com/users/%s/repos"  # % (userid)
+    commit_url = "https://api.github.com/repos/%s/%s/commits"  # % (id, repo)
+
+    # Get User
+    repo_page = requests.get(repo_url % user)
+    if repo_page.status_code == 200:
+        repo_data = repo_page.json()
+        # Parse Repositories
+        for each in repo_data:
+            repo_name = each["name"]
+            # Get Repo Commits
+            commit_page = requests.get(commit_url % (user, repo_name))
+            if commit_page.status_code == 200:
+                commit_data = commit_page.json()
+                print("Repo: ", repo_name, "   Commits: ", len(commit_data))
+            else:
+                print("No Connection: Line 26")
+                code200 = False
+    else:
+        print("No Connection: Line 18")
+        code200 = False
+
+    return code200
+
+
+# Run Program
+def run():
+    user = input("Please enter a GitHub user ID: ")
+    try:
+        connect(user)
+    except(AttributeError):
+        print("Unable to find an account with that username")
+
+
+if __name__ == '__main__':
+    run()
